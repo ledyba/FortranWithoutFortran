339518c296d63299bfbab28b8bae79ef4e68c2b9
psi <ledyba@users.sourceforge.jp>
2014-07-11 21:03:48 +0900

    consider .rodata

--- builder/generate.sh
+++ builder/generate.sh
@@ -2,7 +2,7 @@
 _file=$(cd $(dirname $0); pwd -LP)/calc.so
 _out=$(cd $(dirname $0); pwd -LP)/prog.f95.in
 _start=$(objdump -h calc.so | grep \\s.text\\s | awk '{ print $6 }')
-_size=$(objdump -h calc.so | grep \\s.text\\s | awk '{ print $3 }')
+_size=$(expr $(objdump -h calc.so | grep \\s.rodata\\s | awk '{ print $3 }') \+ $(objdump -h calc.so | grep \\s.rodata\\s | awk '{ print $6 }') \- $_start)
 _ep=$(nm calc.so | grep \\sep$ | awk '{ print $1 }')
 
 cd ../generator
